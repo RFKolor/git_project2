282e479fe8d903295f2be59960dcd0a7258d92b7
RFKolor <https://github.com/RFKolor/Gus-Hrustalniy-Babaskin.git>
2022-12-01 15:28:44 +0300

    second commit

--- main.py
+++ main.py
@@ -3,6 +3,9 @@ def main_function():
     print("My second git program")
     print("And I will change it every day")
 
+def second_function():
+    print("14 декбря самый лучший день")
+
 
 if __name__ == "__main__":
     main_function()
